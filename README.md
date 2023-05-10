3b60341ba4ebfa3c6ec34ff97167047cb74f5dfc
= <clodoaldo.zeferini.enterprise@outlook.com>
2023-05-09 21:59:38 -0300

    Atualizando node

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cz-ent-nodejs-aws-ecs-fargate
 cz-ent-nodejs-aws-ecs-fargate
+
+Sequência de publicação do docker no AWS Fargate
+
+1 - (Get-ECRLoginCommand).Password | docker login --username AWS --password-stdin 504330613280.dkr.ecr.us-east-1.amazonaws.com
+
+2 - docker build -t fargate-nodejs-app .
+
+3 - docker tag fargate-nodejs-app:latest 504330613280.dkr.ecr.us-east-1.amazonaws.com/fargate-nodejs-app:latest
+
+4 - docker push 504330613280.dkr.ecr.us-east-1.amazonaws.com/fargate-nodejs-app:latest
--- src/app/index.js
+++ src/app/index.js
@@ -8,8 +8,19 @@ const port = 3000;
 
 /*endpoints*/
 
+const indexPage = `
+    <h3>Hello from a NodeJS Application running on AWS ECS Fargate</h3>
+    <p>What would you like to see?</p>
+    <ul>
+        <li>Random Dogs - <a href="/dogs"> Click Here</a></li>
+        <li>Random Cats - <a href="/cats"> Click Here</a></li>        
+    </ul>
+
+`;
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  console.log('get Ok');
+  
+  res.send(indexPage);
 });
 
 app.get('/healthcheck', (req, res) => {
@@ -19,6 +30,23 @@ app.get('/healthcheck', (req, res) => {
       res.sendStatus(500);
     }
   });
+
+app.get('/dogs', async (req, res) => {
+  try {
+    const response = await axios.get('https://dog.ceo/api/breeds/image/random');
+
+    console.log(JSON.stringify(response.data));
+
+    const { message: dogImage } = response.data;
+    res.send(
+      `<img src='${dogImage}' alt='random dog' style='max-width: 500px' />`
+    );
+  } catch (error) {
+    console.error(JSON.stringify(error));
+    res.status(500);
+    res.send(error.message);
+  }
+});
 
 app.get('/cats', async (req, res) => {
   try {
